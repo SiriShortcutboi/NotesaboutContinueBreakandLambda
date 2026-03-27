1dceee849789f23a6e2c1c81e1c6adaedec96a01
holden.anderson <holden.anderson@students.snow.edu>
2026-03-27 15:52:21 -0600

    break and continue notes

---
+++ BreakAndContinueNotes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Text.Json;
+					
+public class Program
+{
+	public static void Main()
+	{
+        //break; continue;
+
+        for (int i = 0; i < 10; i++)
+		{
+            Console.WriteLine($"{i}");
+            //code
+            continue;
+            //i represent a person applying to job
+
+            //step one to check if good
+                //step 2
+                    //step 3 fail
+                        //continue is like a skip, it says dont worry about it sweetheart, move on to the next thing.
+
+                            //step 4
+                                //step 5
+                                    //step 6
+            
+            
+             //break is used way more than continue, 
+            //using continue will take the compiler to it's next exectuion rather than finishing that method
+            //so in this case it doesnt print 5, and it goes to its next execution: Printing 6
+            
+            
+            
+            //break; 
+            //break exits You from a loop, 
+            //return exits You from a whole method, 
+            // you'll probably see return more when you have your method by itself down at the bottom
+            
+        }
+	}
+}
